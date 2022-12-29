54a4a13b7b58b36e2bb3be8ed883afa3936ced86
thstnqls <0928ssb@naver.com>
2022-12-29 17:26:47 +0900

    flask_test

--- simpleFlask.py
+++ simpleFlask.py
@@ -1,5 +1,16 @@
-a=10
-b=20
-c=a+b
-print(c)
-print('Hello Flask')
+#필요한 모듈을 단계적으로 import
+from flask import Flask
+ 
+# static 기능 추가
+app = Flask(__name__)
+ 
+# 테스트 기능
+@app.route('/hi')
+def hello_Flask():
+    return 'hi, Flask'
+
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=7777,
+    debug=True)
